2bc4dfba302b5d24e63aff6fc35ea3c0d21be165
DimaKarvackij <dimakarvackij777@gmail.com>
2024-05-18 08:28:15 +0300

    v

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Feedback from "./components/Feedback/Feedback"
 import Options from "./components/Options/Options"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Notification from './components/Notification/Notification'
 import Description from "./components/Description/Description"
 
@@ -9,15 +9,16 @@ import Description from "./components/Description/Description"
 
 
 function App() {
-    const [feedback, setFedback] = useState(() => {
+    const [feedback, setFeedback] = useState(() => {
         const savedFeedback = localStorage.getItem("feedback");
         return savedFeedback
             ? JSON.parse(savedFeedback)
             : { good: 0, neutral: 0, bad: 0 };
+
     })
 
     const updateFeedback = feedbackType => {
-        setFedback({
+        setFeedback({
             ...feedback,
             [feedbackType]: feedback[feedbackType] + 1
         })
@@ -25,11 +26,11 @@ function App() {
     const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
     const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
     const handleReset = () => {
-        setFedback({ good: 0, neutral: 0, bad: 0 });
+        setFeedback({ good: 0, neutral: 0, bad: 0 });
     };
     useEffect(() => {
-        localStorage.setItem("saved-feedback-statistics", JSON.stringify(clicks));
-    }, [clicks]);
+        localStorage.setItem("saved-feedback-statistics", JSON.stringify(feedback));
+    }, [feedback]);
 
 
     return (
